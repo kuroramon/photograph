87b417d613643464c079d5d4252b142bf9e94293
kuroramon <kuroramon@outlook.jp>
2022-08-21 18:57:46 +0900

    add migrate

--- photograph-admin/app/models/photo.go
+++ photograph-admin/app/models/photo.go
@@ -7,7 +7,7 @@ type Photo struct {
 	FileName     string    `gorm:"size:225" json:"fileName,omitempty"`
 	Name         string    `gorm:"size:225" json:"name,omitempty"`
 	Location     string    `gorm:"size:225" json:"location,omitempty"`
-	Color        int       `gorm:"size:500" json:"color,omitempty"`
-	Genre        int       `json:"genre,omitempty"`
+	Color        uint      `gorm:"size:500" json:"color,omitempty"`
+	Genre        uint      `json:"genre,omitempty"`
 	Shootingdate time.Time `json:"shooting_date"`
 }
